bffdd4edbdc7e5cd77d7c037e23c5f4a707377f3
Endi_Trico <enditrico@gmail.com>
2023-05-21 12:55:22 +0200

    Welcome Activity

--- app/src/main/java/com/example/food_recipes_application/WelcomeActivity.java
+++ app/src/main/java/com/example/food_recipes_application/WelcomeActivity.java
@@ -3,10 +3,15 @@ package com.example.food_recipes_application;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    @SuppressLint("QueryPermissionsNeeded")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +20,17 @@ public class WelcomeActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, bottomNavigationFragment);
         transaction.commit();
+
+        TextView textEmail = findViewById(R.id.textViewEmail);
+
+        textEmail.setOnClickListener(v -> {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"info@foodtopia.al"});
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        });
     }
 }
